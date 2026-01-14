aa2080c8d10e156235ff995865961c583c75751d
Darshil Shah <darshils99@gmail.com>
2026-01-14 10:47:18 -0600

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,14 @@
+/**
+ * Core Atlas Configuration Barrel
+ * Central export point for all configuration modules
+ * 
+ * Usage:
+ * ```typescript
+ * import { ENV, validateEnv } from '@/config';
+ * import { TaskCard, DependencyGraph } from '@/config/ui';
+ * import { SYSTEM_CONFIG } from '@/config/system';
+ * ```
+ */
 export * from "./system";
 export * from "./ui";
 export * from "./env";

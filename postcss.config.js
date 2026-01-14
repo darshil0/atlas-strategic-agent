6ee959dec1857ae84e31a0f23ddfdae3472c090c
Darshil Shah <darshils99@gmail.com>
2026-01-14 10:24:23 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+import type { Config } from "postcss";
+
+const config: Config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
+
+export default config;
